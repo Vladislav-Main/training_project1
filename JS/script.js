2a661f4862b2c89a1a45abeefdcfb7eebc8427af
Vladislav-Main <vlados.77772@gmail.com>
2020-11-29 19:39:25 +0200

    First commit

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+// alert('please pay attention');
+// console.log(alert);
+
+const answers = [];
+answers[0] = +prompt('Сколько тебе лет?');
+answers[1] = prompt('Как тебя зовут?');
+answers[2] = prompt('Какая у тебя фамилия?');
+
+console.log(answers);
